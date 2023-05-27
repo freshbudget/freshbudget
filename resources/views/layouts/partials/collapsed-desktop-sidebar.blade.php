048ea5200c7366e073100c15e2b6633151101495
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2023-05-27 00:11:18 -0700

    wip

--- resources/views/layouts/partials/collapsed-desktop-sidebar.blade.php
+++ resources/views/layouts/partials/collapsed-desktop-sidebar.blade.php
@@ -3,27 +3,27 @@
     <!-- Main sidebar links -->
     <div class="flex flex-col items-center flex-1 py-3 space-y-1 text-gray-600" x-data="{}">
 
-        <a href="{{ route('app.index') }}" class="flex items-center justify-center w-10 h-10 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed {{ active('app.index', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="{{ route('app.index') }}" class="flex items-center justify-center w-10 h-10 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed {{ active('app.index', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Dashboard">
             @svg('home', 'w-5 h-5')
         </a>
 
-        <a href="#" class="flex w-10 h-10 justify-center p-2 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed  {{ active('app.calendar.*', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="#" class="flex w-10 h-10 justify-center p-2 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed  {{ active('app.calendar.*', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Calendar">
             @svg('calendar', 'w-5 h-5')
         </a>
 
-        <a href="{{ route('app.incomes.index') }}" class="flex items-center w-10 h-10 justify-center p-2 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed  {{ active('app.incomes.*', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="{{ route('app.incomes.index') }}" class="flex items-center w-10 h-10 justify-center p-2 relative focus:outline-none border border-transparent focus:border-gray-300 focus:bg-gray-100 rounded-lg tracking-tight leading-relaxed  {{ active('app.incomes.*', 'font-semibold text-gray-900') }}" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Incomes">
             @svg('banknotes', 'w-5 h-5')
         </a>
 
-        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Expenses">
             @svg('creditcard', 'w-5 h-5')
         </a>
 
-        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Accounts">
             @svg('bank', 'w-5 h-5')
         </a>
 
-        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()">
+        <a href="#" class="relative flex items-center justify-center w-10 h-10 p-2 leading-relaxed tracking-tight border border-transparent rounded-lg focus:outline-none focus:border-gray-300 focus:bg-gray-100" x-on:mouseenter="$focus.focus($el)" x-on:mouseleave="$el.blur()" title="Transactions">
             @svg('pencilsquare', 'w-5 h-5')
         </a>
 
@@ -32,11 +32,11 @@
     <!-- User profile menu -->  
     <div class="flex flex-col items-center justify-center pb-2 space-y-1">   
 
-        <a href="#" class="relative flex items-center w-10 h-10 max-w-full p-2 leading-relaxed tracking-tight truncate border border-transparent rounded-lg hover:bg-gray-100/90 focus:outline-none hover:shadow-sm hover:border-gray-300 focus:border-gray-300 focus:bg-gray-100">
+        <a href="{{ route('app.settings.personal') }}" class="relative flex items-center w-10 h-10 max-w-full p-2 leading-relaxed tracking-tight truncate border border-transparent rounded-lg hover:bg-gray-100/90 focus:outline-none hover:shadow-sm hover:border-gray-300 focus:border-gray-300 focus:bg-gray-100" title="View account settings">
             @svg('profile', 'w-5 h-5')
         </a>
 
-        <button x-on:click="desktopSidebarExpanded=true" class="relative flex items-center w-10 h-10 max-w-full p-2 leading-relaxed tracking-tight truncate border border-transparent rounded-lg hover:bg-gray-100/90 focus:outline-none hover:shadow-sm hover:border-gray-300 focus:border-gray-300 focus:bg-gray-100">
+        <button x-on:click="desktopSidebarExpanded=true" class="relative flex items-center w-10 h-10 max-w-full p-2 leading-relaxed tracking-tight truncate border border-transparent rounded-lg hover:bg-gray-100/90 focus:outline-none hover:shadow-sm hover:border-gray-300 focus:border-gray-300 focus:bg-gray-100" title="Expand sidebar">
             @svg('chevron-double-right', 'w-5 h-5')
         </button>
         
